10bf21838062de08e7d6e5bcbd7f850ab2e0772a
niten <niten@fudo.org>
2025-06-10 22:08:29 -0700

    fix build

--- src/clj/klaxon/cli/core_test.clj
+++ src/clj/klaxon/cli/core_test.clj
@@ -1,10 +1,10 @@
 (ns klaxon.cli.core-test
   (:require [clojure.tools.cli :as cli]
             [clojure.test :refer :all]
-            [klaxon.cli.core :as cli]))
+            [klaxon.cli.core :as c]))
 
 (deftest test-parse-opts
   (testing "Parse CLI options with required arguments"
     (let [args ["-k" "keyfile.json" "-t" "topic" "-p" "60"]
-          result (cli/parse-opts args #{:key-file :ntfy-topic :poll-seconds} cli/cli-opts)]
+          result (cli/parse-opts args #{:key-file :ntfy-topic :poll-seconds} c/cli-opts)]
       (is (empty? (:errors result))))))
