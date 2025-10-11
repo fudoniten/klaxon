9b003ed5f777ab8c68551a987ab3878bf27e9c51
fudoniten <niten@fudo.org>
2025-10-10 19:44:57 -0700

    Fix CLI parse opts test

--- test/klaxon/cli/core_test.clj
+++ test/klaxon/cli/core_test.clj
@@ -1,10 +1,9 @@
 (ns klaxon.cli.core-test
-  (:require [clojure.tools.cli :as cli]
-            [clojure.test :refer :all]
+  (:require [clojure.test :refer :all]
             [klaxon.cli.core :as c]))
 
 (deftest test-parse-opts
   (testing "Parse CLI options with required arguments"
-    (let [args ["-k" "keyfile.json" "-t" "topic" "-p" "60"]
-          result (cli/parse-opts args #{:key-file :ntfy-topic :poll-seconds} c/cli-opts)]
+      (let [args ["-k" "keyfile.json" "-t" "topic" "-p" "60"]
+            result (#'c/parse-opts args #{:key-file :ntfy-topic :poll-seconds} c/cli-opts)]
       (is (empty? (:errors result))))))
